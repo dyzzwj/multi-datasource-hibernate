08b877c4b64670f30022f4a9e7842c07d881b58c
dyzzwj <dyzwj1998@163.com>
2020-09-04 11:20:54 +0800

    0904

---
+++ src/main/java/com/dyzwj/multidatasourcehibernate/MultiDatasourceHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.dyzwj.multidatasourcehibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDatasourceHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiDatasourceHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dyzwj/multidatasourcehibernate/MultiDatasourceHibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dyzwj.multidatasourcehibernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultiDatasourceHibernateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
